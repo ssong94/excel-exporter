964ff0f3d9e67460c97c94e337cdabfa67124f68
ssong94 <kyugwang@gmail.com>
2024-04-14 23:47:44 +0900

    init

---
+++ src/main/java/com/spring/boot/excelexporter/ExcelExporterApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.excelexporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelExporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelExporterApplication.class, args);
+	}
+
+}
