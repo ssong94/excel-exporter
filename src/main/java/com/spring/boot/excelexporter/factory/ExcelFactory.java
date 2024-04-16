7ee87fef32922429e34ffc9151251251f55aa1a3
ssong94 <kyugwang@gmail.com>
2024-04-17 00:34:49 +0900

    add: factory class

---
+++ src/main/java/com/spring/boot/excelexporter/factory/ExcelFactory.java
@@ -0,0 +1,15 @@
+package com.spring.boot.excelexporter.factory;
+
+import com.spring.boot.excelexporter.container.ClassContainer;
+import com.spring.boot.excelexporter.container.ExcelExporter;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ExcelFactory {
+	public static <T> ExcelExporter createExcel(List<T> data, Class<T> tClass) {
+		return ClassContainer.from(data, tClass);
+	}
+
+}
