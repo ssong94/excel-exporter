8c3872e79ddbc85dd2c141c8573bfc2dbf3965e9
ssong94 <kyugwang@gmail.com>
2024-04-21 21:00:15 +0900

    test: 테스트 코드 작성

--- src/main/java/com/spring/boot/excelexporter/test/TestVo.java
+++ src/main/java/com/spring/boot/excelexporter/test/TestVo.java
@@ -3,21 +3,60 @@ package com.spring.boot.excelexporter.test;
 import com.spring.boot.excelexporter.meta.ExcelBody;
 import com.spring.boot.excelexporter.meta.ExcelHeader;
 import com.spring.boot.excelexporter.meta.ExcelSheet;
+import com.spring.boot.excelexporter.meta.style.ExcelAlignmentStyle;
+import com.spring.boot.excelexporter.meta.style.ExcelBackgroundStyle;
+import com.spring.boot.excelexporter.meta.style.ExcelFontStyle;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
+import org.apache.poi.hssf.util.HSSFColor.HSSFColorPredefined;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.IndexedColors;
 
 @ExcelSheet(name = "시트명")
 @AllArgsConstructor
 public class TestVo {
 
-	@ExcelHeader(name = "헤더명이 너무 길면 어떻게 되지", order = 0)
-	@ExcelBody(order = 0)
-	private int column1;
+	@ExcelHeader(name = "number", order = 0)
+	@ExcelBody(order = 0, dataFormat = "0;[Red]-0")
+	private int number;
 
-	@ExcelHeader(name = "헤더명2", order = 1)
+	@ExcelHeader(name = "default", order = 1)
 	@ExcelBody(order = 1)
-	private String column2;
+	private String value;
+
+	@ExcelHeader(name = "percent", order = 2)
+	@ExcelBody(order = 2, dataFormat = "0%;[Red]-0")
+	private double percent;
+
+	@ExcelHeader(name = "Date", order = 3)
+	@ExcelBody(order = 3, dataFormat = "yy-m-d")
+	private LocalDate localDate;
+
+	@ExcelHeader(name = "DateTime", order = 4, width = 10.00f)
+	@ExcelBody(order = 4, dataFormat = "yy.m.d h:mm",
+			alignmentStyle = @ExcelAlignmentStyle(
+					horizontalAlign = HorizontalAlignment.LEFT
+			)
+	)
+	private LocalDateTime localDateTime;
+
+	@ExcelHeader(name = "Custom", order = 5,
+	backgroundStyle = @ExcelBackgroundStyle(
+			color = IndexedColors.BROWN),
+			fontStyle = @ExcelFontStyle(
+					fontSize = 13,
+					fontName = "verdana",
+					bold = true,
+					underline = Font.U_SINGLE,
+					fontColor = HSSFColorPredefined.YELLOW))
+	@ExcelBody(order = 5, dataFormat = "yy/m/d h:mm")
+	private String customValue;
+
+	@ExcelHeader(name = "DateTime", order = 6, width = 10.00f)
+	@ExcelBody(order = 6, dataFormat = "$#,##0;[Blue]-$#,##0")
+	private int won;
 
 
-	@ExcelBody(order = 2)
-	private String column3;
 }
--- src/test/java/com/spring/boot/excelexporter/ExcelExportHelperApplicationTests.java
+++ src/test/java/com/spring/boot/excelexporter/ExcelExportHelperApplicationTests.java
@@ -1,9 +1,15 @@
 package com.spring.boot.excelexporter;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import com.spring.boot.excelexporter.container.ExcelExporter;
 import com.spring.boot.excelexporter.factory.ExcelFactory;
 import com.spring.boot.excelexporter.test.TestVo;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -15,17 +21,27 @@ class ExcelExportHelperApplicationTests {
 	@org.junit.jupiter.api.Test
 	void excelCreateTest() throws IOException {
 
-		TestVo testVo = new TestVo(1, "data1", "data2");
-		TestVo testVo1 = new TestVo(2, "data3", "data4");
-		TestVo testVo2 = new TestVo(3, "데이터가 너무 길면", "data5");
-		List<TestVo> testVo3 = List.of(testVo, testVo1, testVo2);
+		TestVo testVo = new TestVo(-1234, "data1", 0.1, LocalDate.now(), LocalDateTime.now(), "value", 1);
+		TestVo testVo1 = new TestVo(0, "data1", 0.2, LocalDate.now(), LocalDateTime.now(), "value", 12);
+		TestVo testVo2 = new TestVo(-1, "data1", 0.3, LocalDate.now(), LocalDateTime.now(), "value", -1313);
+		TestVo testVo3 = new TestVo(31, "data1", 0.5, LocalDate.now(), LocalDateTime.now(), "value", 155);
+		TestVo testVo4 = new TestVo(124, "data1", 0.7, LocalDate.now(), LocalDateTime.now(), "value", 166);
+		TestVo testVo5 = new TestVo(234, "data1", 0.8, LocalDate.now(), LocalDateTime.now(), "value", -10110);
+		TestVo testVo6 = new TestVo(2, "data1", 0.9, LocalDate.now(), LocalDateTime.now(), "value", 545);
+		TestVo testVo7 = new TestVo(3, "data1", 1, LocalDate.now(), LocalDateTime.now(), "value", 10000);
+		TestVo testVo8 = new TestVo(-1010, "data1", 2, LocalDate.now(), LocalDateTime.now(), "value", -12323);
+		TestVo testVo9 = new TestVo(1010, "data1", 3, LocalDate.now(), LocalDateTime.now(), "value", 0);
+		TestVo testVo10 = new TestVo(9999, "data1", 0.4, LocalDate.now(), LocalDateTime.now(), "value", 999999);
+		TestVo testVo11 = new TestVo(-9999, "data1", 0.04, LocalDate.now(), LocalDateTime.now(), "value", -1010);
+		TestVo testVo12 = new TestVo(10000, "data1", 0.03, LocalDate.now(), LocalDateTime.now(), "value", 1010);
 
+		List<TestVo> list = List.of(testVo, testVo1, testVo2, testVo3, testVo4, testVo5, testVo6, testVo7, testVo8,
+				testVo9, testVo10, testVo11, testVo12);
 
-		ExcelExporter excel = ExcelFactory.makeExcel(testVo3, TestVo.class);
-
+		ExcelExporter excel = ExcelFactory.makeExcel(list, TestVo.class);
 		excel.save(path);
-		// save 후 resource 반환함.
 
+		assertTrue(Files.exists(Path.of(path)));
 
 	}
 }
